a99c464b91dbec17f6e7aadce9e6fa4cee004101
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-10-24 17:19:45 +0200

    fixat backend efter lärarnvisningar. Routes + testfil

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
+const port = process.env.port || 3001
 app.use(express.json());
 
-// define a route to handle GET requests to /api/game
-app.get('/api/game', (req, res) => {
-  // read the game data from db.json
-  const gameData = require('./db.json');
-  res.json(gameData);
-});
+// app.use((req,res)=>{
+//   console.log({query:req.query});
+//   console.log({body: req.body});
+//   console.log({params: req.params});
+//   console.log({header: req.headers});
+//   res.send({status:"succes1s"});
+// })
 
-// define a route to handle POST requests to /api/game
-app.post('/api/game', (req, res) => {
-  // update the game data in db.json
-  const gameData = req.body;
-  // TODO: write code to update db.json with the new game data
-  res.json(gameData);
-});
+app.use('/api/gomoku', require('./gomoku_routes'))
 
-// start the server
+// // define a route to handle GET requests to /api/game
+// app.get('/api/game', (req, res) => {
+//   // read the game data from db.json
+//   const gameData = require('./db.json');
+//   res.json(gameData);
+// });
+// // define a route to handle POST requests to /api/game
+// app.post('/api/game', (req, res) => {
+//   // update the game data in db.json
+//   const gameData = req.body;
+//   // TODO: write code to update db.json with the new game data
+//   res.json(gameData);
+// });
+
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`http server listening on port http://localhost:${port}/api/gomoku/play`);
 });
