01c59ee67863d4ffe098d696f8d1dc6a798f7f9f
leeparksara <90184658+leeparksara@users.noreply.github.com>
2023-10-27 14:31:24 +0900

    skapade hemsida, hero sektion

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import Section from './components/Section.jsx'
+
+//import logo from './logo.svg';
 import './App.css';
-import Board from './components/Board';
+//import Board from './components/Board';
 
 function App() {
   return (
     <div className="App">
-      <header>
+
+<main >
+       <Section className="main-container" />
+      </main>
+    {/*  <header>
         <img src={logo} className="GomokuGurus-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
-      <Board className="board-container" />
+      <Board className="board-container" />*/}
+    
     </div>
   );
 }
