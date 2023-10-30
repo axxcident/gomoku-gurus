5303c86f40f9facf9afaec2bc55ef9552a2c612b
Maximilian Bakaldin <maximilian.bakaldin@gmail.com>
2023-10-30 16:33:41 +0100

    log in modal design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 import Testboard from './components/Testboard';
 import Board from './components/Board';
 import Navbar from './components/Navbar';
+import Modal from './components/LogInModal';
 // import { Route, Routes } from "react-router-dom";
 
 function App() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+      setIsModalOpen(true);
+    }
+
+    const closeModal = () => {
+      setIsModalOpen(false);
+    }
+
   return (
     <>
       {/* <Routes>
@@ -13,7 +25,8 @@ function App() {
         <Route path='/gameplay' element={ <Gameplay /> } />
         <Route path='/contact' element={ <Contact /> } />
       </Routes> */}
-      <Navbar />
+      <Navbar openModal={openModal} />
+      <Modal isOpen={isModalOpen} closeModal={closeModal} />
       <Testboard className="board-container" />
       <Board />
     </>
