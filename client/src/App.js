96f8c1eea254ed3c51a88e452f0d10b8d2d04258
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-10-30 16:30:01 +0100

    synkat Saras ändringar i DEV

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,24 @@
 import React from 'react'
-import Section from './components/Section.jsx'
+import Home from './pages/Home.jsx'
 import './App.css';
-import Testboard from './components/Testboard';
-import Board from './components/Board';
+// import Testboard from './components/Testboard';
+// import Board from './components/Board';
 import Navbar from './components/Navbar';
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Routes>
-        <Route path='/' element={ <Home /> } />
-        <Route path='/about' element={ <About /> } />
-        <Route path='/gameplay' element={ <Gameplay /> } />
-        <Route path='/contact' element={ <Contact /> } />
-      </Routes> */}
       <Navbar />
-    <div className="App">
-      <main>
-       <Section className="main-container" />
-      </main>
-    </div>
-      <Testboard className="board-container" />
-      <Board />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        {/* <Route path='/about' element={ <About /> } />
+        <Route path='/gameplay' element={ <Gameplay /> } />
+        <Route path='/contact' element={ <Contact /> } /> */}
+      </Routes>
+       {/* <Section className="main-container" /> */}
+      {/* <Testboard className="board-container" />
+      <Board /> */}
     </>
   );
 }
--- client/src/components/Section.jsx
+++ client/src/components/Section.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 
-const Section = ()=>{
+const Section = () => {
 return (
-    
-<div className="main-content">
-<div className="hero-text">
-<h3>
-Omuku –  <br/>
-Där Strategi Möter Spänning och Skicklighet!"
-</h3>
-<h5>
-Utforska Ditt Strategiska Skicklighet <br/>
- i Det Perfekta Omoku-Äventyret!
-</h5>
-</div>
- 
-<div>
-
-<input className="start-button"  type="button" value="Börja spela" />
-
-
-
-</div>
-
+    <div className="main-content">
+      <div className="hero-text">
+        <h3>
+            Omuku –  <br/>
+            Där Strategi Möter Spänning och Skicklighet!"
+        </h3>
+        <h5>
+        Utforska Ditt Strategiska Skicklighet <br/>
+        i Det Perfekta Omoku-Äventyret!
+        </h5>
+      </div>
+      <div>
+        <input className="start-button"  type="button" value="Börja spela" />
+      </div>
     </div>
-
-    
-)
-}
+)}
 
 export default Section;
