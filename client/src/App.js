7da238b16b97ff0587a3f0ef3f989a9a5b5277f0
Linus3401 <112473172+Linus3401@users.noreply.github.com>
2023-10-31 10:37:02 +0100

    sparad kod

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 // import Board from './components/Board';
 import Testboard from './components/Testboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="GomokuGurus-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      <Testboard className="board-container" />
-    </div>
+    <BrowserRouter>
+      <>
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+        <div className="App">
+          <header>
+            <img src={logo} className="GomokuGurus-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+          </header>
+          <Testboard className="board-container" />
+        </div>
+      </>
+    </BrowserRouter>
   );
 }
 
