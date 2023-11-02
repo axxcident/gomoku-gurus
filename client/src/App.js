82c7a212694f309dab87c1fd7eaa42d293f803d9
leeparksara <90184658+leeparksara@users.noreply.github.com>
2023-11-03 02:38:50 +0900

    Spelsida behöver lite fix i css

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import SpelSida from './pages/SpelSida.jsx';
 import './App.css';
 // import Testboard from './components/Testboard';
 // import Board from './components/Board';
@@ -24,6 +25,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Home /> } />
         <Route path='/about' element={ <About /> } />
+        <Route path='/spelSida' element={<SpelSida/>} />
         {/* <Route path='/gameplay' element={ <Gameplay /> } />
         <Route path='/contact' element={ <Contact /> } />  */}
       </Routes>
