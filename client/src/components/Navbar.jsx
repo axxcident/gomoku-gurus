72530208897d708b6d513e43b6fc0c92cb449061
Linus3401 <112473172+Linus3401@users.noreply.github.com>
2023-11-06 13:59:40 +0100

    spelregler klar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = ({ openModal }) => {
+    
 
     const testclick = () => {
         const dropdownContent = document.getElementById('dropdownContent');
