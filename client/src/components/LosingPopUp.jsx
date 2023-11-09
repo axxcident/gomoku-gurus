d8c560a26bff89b6b8c86d19d1b49f6d8153a477
Maximilian Bakaldin <maximilian.bakaldin@gmail.com>
2023-11-09 14:19:36 +0100

    test-update Max

--- client/src/components/LosingPopUp.jsx
+++ client/src/components/LosingPopUp.jsx
@@ -2,28 +2,28 @@ import React, {useState} from 'react'
 
 
 function LosingPopUp(){
-   
+
 const [isLosingPopupHidden, SetLosingPopupHidden] = useState(true)
 const handleClick = ()=>{
   SetLosingPopupHidden(false)
 
 }
 
-    
+
   return isLosingPopupHidden ? (
-    
-    <div className='losing-popup'>
+
+    <div className='losing-popup' data-testid="losing-popup">
    <div className='losing-popup-content'><button id='close-popup' onClick={handleClick}>✖</button>
 
 
 <p>Du förlorade! </p>
-    
+
     <button className='losing-popup-button'>Försök igen?</button>
-  
+
     </div>
-    
+
   </div>
-  
+
   ) :null;
 }
 
